550c4585b1ce7f60a5b2bd460b84533d50d30e8d
Pat613 <102959407+Patrickyeung613@users.noreply.github.com>
2025-06-13 01:55:23 +0800

    Add file

---
+++ game_data.py
@@ -0,0 +1,128 @@
+data = [
+    {
+        'name': 'Instagram',
+        'follower_count': 346,
+        'description': 'Social media platform',
+        'country': 'United States'
+    },
+    {
+        'name': 'Facebook',
+        'follower_count': 2000,
+        'description': 'Social media platform',
+        'country': 'United States'
+    },
+    {
+        'name': 'Twitter',
+        'follower_count': 330,
+        'description': 'Microblogging platform',
+        'country': 'United States'
+    },
+    {
+        'name': 'TikTok',
+        'follower_count': 1200,
+        'description': 'Short video platform',
+        'country': 'China'
+    },
+    {
+        'name': 'YouTube',
+        'follower_count': 1500,
+        'description': 'Video sharing platform',
+        'country': 'United States'
+    },
+    {
+        'name': 'Snapchat',
+        'follower_count': 500,
+        'description': 'Multimedia messaging app',
+        'country': 'United States'
+    },
+    {
+        'name': 'LinkedIn',
+        'follower_count': 700,
+        'description': 'Professional networking site',
+        'country': 'United States'
+    },
+    {
+        'name': 'Reddit',
+        'follower_count': 430,
+        'description': 'Social news aggregation',
+        'country': 'United States'
+    },
+    {
+        'name': 'Pinterest',
+        'follower_count': 450,
+        'description': 'Image sharing and social media',
+        'country': 'United States'
+    },
+    {
+        'name': 'WeChat',
+        'follower_count': 1200,
+        'description': 'Messaging and social media app',
+        'country': 'China'
+    },
+    {
+        'name': 'WhatsApp',
+        'follower_count': 1800,
+        'description': 'Messaging app',
+        'country': 'United States'
+    },
+    {
+        'name': 'Quora',
+        'follower_count': 300,
+        'description': 'Question-and-answer platform',
+        'country': 'United States'
+    },
+    {
+        'name': 'Vimeo',
+        'follower_count': 200,
+        'description': 'Video hosting platform',
+        'country': 'United States'
+    },
+    {
+        'name': 'Tumblr',
+        'follower_count': 400,
+        'description': 'Microblogging platform',
+        'country': 'United States'
+    },
+    {
+        'name': 'Flickr',
+        'follower_count': 250,
+        'description': 'Image and video hosting',
+        'country': 'United States'
+    },
+    {
+        'name': 'Discord',
+        'follower_count': 900,
+        'description': 'Voice and text chat platform',
+        'country': 'United States'
+    },
+    {
+        'name': 'Clubhouse',
+        'follower_count': 600,
+        'description': 'Audio chat application',
+        'country': 'United States'
+    },
+    {
+        'name': 'Telegram',
+        'follower_count': 700,
+        'description': 'Cloud-based messaging app',
+        'country': 'Russia'
+    },
+    {
+        'name': 'MySpace',
+        'follower_count': 150,
+        'description': 'Social networking site',
+        'country': 'United States'
+    },
+    {
+        'name': 'Meetup',
+        'follower_count': 100,
+        'description': 'Event organizing platform',
+        'country': 'United States'
+    },
+    {
+        'name': 'Badoo',
+        'follower_count': 300,
+        'description': 'Dating-focused social network',
+        'country': 'United Kingdom'
+    }
+]
---
+++ main.py
@@ -0,0 +1,50 @@
+import random
+import art
+from game_data import data
+
+data_list = data
+
+def check_list():
+    if len(data_list) < 2:
+        return 2
+    
+def compare():
+    if check_list() == 2:
+        return 2
+    data_A = random.choice(data_list)
+    print(f"Compare A: {data_A['name']}, {data_A['description']}.")
+    data_list.remove(data_A)
+
+    print(art.vs)
+
+    data_B = random.choice(data_list)
+    print(f"Compare B: {data_B['name']}, {data_B['description']}.")
+    data_list.remove(data_B)
+
+    choice = input("Who has more follower? Type 'A' or 'B':").upper()
+    if (choice == "A" and data_A['follower_count'] > data_B['follower_count']) or (choice == "B" and data_B['follower_count'] > data_A['follower_count']):
+        return 1
+    else:
+        return 0
+    
+def play_game(): 
+    result = -1
+    score = 0
+    print(art.logo)
+
+    while result != 0:
+      result = compare()
+      if result == 1:
+          score += 1
+          print("\n" * 20)
+          print(art.logo)
+          print(f"You are right! Current score: {score}")
+      elif result == 2:
+          print(f"No more data to compare, You win! Final score: {score}")
+          return 
+      else:
+          print(f"Sorry, that's wrong. Final score: {score}")
+          return
+
+
+play_game()
